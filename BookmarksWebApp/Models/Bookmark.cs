2902ca3168c7fca0bcac40c1ffbd99b453900ccc
stevtrem <steven.tremblayb@hotmail.com>
2021-01-28 13:40:28 -0500

    Add class Bookmark

---
+++ BookmarksWebApp/Models/Bookmark.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookmarksWebApp.Models
+{
+    public class Bookmark
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        [Url]
+        public string Url { get; set; }
+    }
+}
